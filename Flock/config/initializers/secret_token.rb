c5ea0b16d917925fff98dbf8868b96bdc7fa90c5
Adam Waxman <awaxman11@gmail.com>
2013-03-19 12:02:08 -0400

    Update for deleted files

--- Flock/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Flock::Application.config.secret_token = '9fd493cdf8571a202bb3308eee23f210717f1622e9f3aac90474d661fb63881dc26775d4bab284a586444c1ae3df57fd4469b9d302f2540e96a6b9eaa2fe7c1f'
