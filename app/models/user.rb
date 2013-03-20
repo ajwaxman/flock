2785c0de27b9646551d7b68b2364a86cd702dc84
Adam Waxman <awaxman11@gmail.com>
2013-03-20 17:00:26 -0400

    remove email bc not working on heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ActiveRecord::Base
   	attr_accessible :email
 
-  	after_save :signup_welcome_email
+  	# after_save :signup_welcome_email
 
-  	private
-  	def signup_welcome_email 
-  		UserMailer.sign_up(self).deliver
-	end
+ #  	private
+ #  	def signup_welcome_email 
+ #  		UserMailer.sign_up(self).deliver
+	# end
 end
